6010772c8be57cb31dc9719d7b9dc1ebf858c659
Eason710085 <wshunjie8@gmail.com>
2021-10-26 14:43:51 -0400

    Oct 26

--- CS311/Assignment_2/src/Driver.java
+++ CS311/Assignment_2/src/Driver.java
@@ -12,7 +12,48 @@ public class Driver {
   public static String birthday = null;
 
   public static void main(String[] args) {
-    personInfoFinder();
+    Option();
+  }
+
+  public static void Option() {
+    System.out.println("Enter Y to run, N to quit");
+    Scanner scanner = new Scanner(System.in);
+    String option;
+    option = scanner.nextLine();
+    if (option.equals("Y")) {
+      int optNum;
+      System.out.println("1. List everyone in the database.");
+      System.out.println("2. Enter new person to the database.");
+      System.out.println("3. Search person born in a given month");
+      System.out.println("4. Modified the current person in the database.");
+      Scanner scan = new Scanner(System.in);
+      optNum = scan.nextInt();
+      try {
+        switch (optNum) {
+          case 1:
+            System.out.println("Here is the people in the database.");
+            break;
+          case 2:
+            System.out.println("Enter this person's Name: ");
+            break;
+          case 3:
+            System.out.println("Enter the born month of which person you want to search:");
+            break;
+        }
+      } catch (Exception e) {
+        System.out.println("Please enter number only.");
+        e.printStackTrace();
+      }
+
+    } else if (option.equals("N")) {
+      System.out.println("You have exit the program.");
+      System.exit(0);
+    } else {
+      System.out.println("Please enter correct option.");
+      Option();
+    }
+
+
   }
 
   public static void personInfoFinder() {
